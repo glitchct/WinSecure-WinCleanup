a1aa7351a604594f3acb207f3a354c8bb1822d0d
GLITCHCraftTech | GLITCHCT <serverr1@outlook.de>
2026-04-15 14:35:58 +0200

    Create WinSecure-WinCleanup-Template.bat
    Added a template batch file for WinSecure and WinCleanup.

---
+++ main/WinSecure-WinCleanup-Template.bat
@@ -0,0 +1,14 @@
+## This is a Template file for WinSecure and WinCleanup.
+## No License agrees here. But i am not responsible for any kind of damage. Proceed at your own Risk.
+
+@echo off
+title WinSecure-WinCleanup
+
+:main
+echo Welcome to WinSecure and WinCleanup. Please chose a Option. 
+echo.
+echo 1) Your Text here.
+echo 2) Your Text here.
+echo 3) Your Text here.
+echo.
+set /p input=">> "
